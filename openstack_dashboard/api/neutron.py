37b17b7b147f440aea67dcd59ddfed6b9da919f9
Dean Troyer <dtroyer@users.noreply.github.com>
2018-08-27 14:38:11 -0500

    Revert "Remove WRS additions to API wrappers"
    This reverts commit ebdd8ac420af834e30a7b7694406cd71c88d379a.

--- openstack_dashboard/api/neutron.py
+++ openstack_dashboard/api/neutron.py
@@ -1687,6 +1687,86 @@ def get_feature_permission(request, feature, operation=None):
     return True
 
 
+def provider_network_type_list(request, **params):
+    providernet_types = neutronclient(request).list_providernet_types(
+        **params).get(
+            'providernet_types')
+    return [ProviderNetworkType(t) for t in providernet_types]
+
+
+def provider_network_create(request, **kwargs):
+    body = {'providernet': kwargs}
+
+    providernet = neutronclient(request).create_providernet(body=body).get(
+        'providernet')
+    return ProviderNetwork(providernet)
+
+
+def provider_network_list(request, **params):
+    providernets = neutronclient(request).list_providernets(**params).get(
+        'providernets')
+    return [ProviderNetwork(n) for n in providernets]
+
+
+def provider_network_list_for_tenant(request, tenant_id, **params):
+    return provider_network_list(request, tenant_id=tenant_id, **params)
+
+
+def provider_network_list_tenant_networks(request, providernet_id, **params):
+    nets = neutronclient(request).list_networks_on_providernet(
+        providernet_id, **params).get('networks')
+    return [ProviderTenantNetwork(n) for n in nets]
+
+
+def provider_network_get(request, providernet_id,
+                         expand_subnet=True, **params):
+    providernet = neutronclient(request).show_providernet(
+        providernet_id, **params).get('providernet')
+    return ProviderNetwork(providernet)
+
+
+def provider_network_delete(request, providernet_id):
+    neutronclient(request).delete_providernet(providernet_id)
+
+
+def provider_network_modify(request, providernet_id, **kwargs):
+    body = {'providernet': kwargs}
+    providernet = neutronclient(request).update_providernet(
+        providernet_id, body=body).get('providernet')
+    return ProviderNetwork(providernet)
+
+
+def provider_network_range_create(request, **kwargs):
+    body = {'providernet_range': kwargs}
+    range = neutronclient(request).create_providernet_range(body=body).get(
+        'providernet_range')
+    return ProviderNetworkRange(range)
+
+
+def provider_network_range_list(request, **params):
+    ranges = neutronclient(request).list_providernet_ranges(**params).get(
+        'providernet_ranges')
+    return [ProviderNetworkRange(r) for r in ranges]
+
+
+def provider_network_range_get(request, range_id,
+                               expand_subnet=True, **params):
+    range = neutronclient(request).show_providernet_range(
+        range_id, **params).get('providernet_range')
+    return ProviderNetworkRange(range)
+
+
+def provider_network_range_delete(request, range_id):
+    neutronclient(request).delete_providernet_range(range_id)
+
+
+def provider_network_range_modify(request, range_id, **kwargs):
+    body = {'providernet_range': kwargs}
+    range = neutronclient(request).update_providernet_range(
+        range_id, body=body).get('providernet_range')
+    return ProviderNetworkRange(range)
+
+
 def qos_list(request):
     qoses = neutronclient(request).list_qoses().get('qoses')
     return [QoSPolicy(q) for q in qoses]
